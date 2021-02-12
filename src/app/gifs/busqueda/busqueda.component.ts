1454c5aa514a73e4e0306af7462b58cae2a4711a
AariasCR <alanjarias@hotmail.com>
2021-02-12 15:01:13 -0600

    Componente Busqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html'
 })
-export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+export class BusquedaComponent {
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
