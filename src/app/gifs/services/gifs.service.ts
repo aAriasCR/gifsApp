d9a56c6b130c94a3acc18844505f1a767275a663
AariasCR <alanjarias@hotmail.com>
2021-02-16 09:20:00 -0600

    Consumo de Api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,15 +6,29 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apykey: string =  "gEHdzC6tlGloVZkswWUybC9tIc1yeuES";
   private _historial: string[] = [];
 
   get historial(){
     return [...this._historial];
   }
 
+  constructor(private http: HttpClient ){
+    
+  }
+
   buscarGifs(query: string){
-    this._historial.unshift(query);
-    console.log(this._historial);
+
+    query = query.trim().toLowerCase();
+
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=gEHdzC6tlGloVZkswWUybC9tIc1yeuES&q=dbz&limit=10')
+      .subscribe((resp:any) =>{
+        console.log(resp.data);
+      });
   }
 
 }
